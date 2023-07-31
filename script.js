c21f07eff4e9abf16861bac809f4bed43829548c
unknown <amgwazo@hotmail.com>
2023-07-31 20:55:33 +0200

    Add script.js

--- script.js
+++ script.js
@@ -6,21 +6,28 @@ const messages = ['A Flea can jump 350 times its body length.',
 'Slugs have 4 noses.',
 'Only female mosquitoes bite',
 'Polar bear skin is black!',
-'The only mammal capable of flight is the bat.',]
+'The only mammal capable of flight is the bat.'];
+  //array greeting text
+const greetingText =  [
+        'WELCOME TRIVIA: ', 'TIP OF THE DAY: ', 'DID YOU KNOW THAT... '
+    ];
 
-//variable to store random message
-const displayRandomMessage = (arr) => {
-    let rnd = Math.floor(Math.random() * (arr.length));
-    console.log(`TIP FOR THE DAY:
-        ${arr[rnd]} `);
+    const concludingText = "Welcome to nature's lane...";
+
+  //function to return random greeting text
+const rndGreeting = (arr) =>  arr[Math.floor(Math.random() * arr.length)];
+
+
+//function to display random message
+const displayRandomMessage = (headerArr, detailArr, footerStr) => {
+
+    let rnd = Math.floor(Math.random() * (detailArr.length));
+    console.log(`
+        ${headerArr} 
+        ${detailArr[rnd].toUpperCase()}
+        ${footerStr}`);
 }
 
 //run the program
+ displayRandomMessage(rndGreeting(greetingText), messages, concludingText);
 
-displayRandomMessage(messages);
-// let i = 500;
-// for(let i = 1; i <= 500; i++)
-// {
-// displayRandomMessage(messages);
-
-// }
